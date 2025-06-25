519dfae92a8f87e10d3187d2445c1d0c3e0d4ae3
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-06-25 13:48:54 -0400

    added basic functions for add,sub,multiply,divide

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let add = function(x,y){
+    return x + y;
+};
+
+let sub = function(x,y){
+    return x - y;
+};
+
+let mult = function(x,y){
+    return x * y;
+};
+
+let div = function(x,y){
+    return x / y;
+};
+
